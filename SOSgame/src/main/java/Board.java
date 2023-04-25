b66285b2f5fa367491db4c2c09477e4273d889c5
alexmzztt <88528696+alexmzztt@users.noreply.github.com>
2023-04-25 18:18:06 -0500

    BoardGUI

---
+++ SOSgame/src/main/java/Board.java
@@ -0,0 +1,9 @@
+public class Board {
+    public String getCell(int row, int column) {
+        return null;
+    }
+
+    public String getTurn() {
+        return null;
+    }
+}
---
+++ SOSgame/src/main/java/BoardGUI.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class BoardGUI {
+    private JRadioButton simpleGameRadioButton;
+    private JRadioButton generalGameRadioButton;
+    private JPanel panel1;
+    private JTextField textField1;
+    private JRadioButton sRadioButton;
+    private JRadioButton oRadioButton;
+    private JRadioButton sRadioButton1;
+    private JRadioButton oRadioButton1;
+    private JButton newGameButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
